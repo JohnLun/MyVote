8ed1fc2374bc6734e0acebe5a88fe608aad04f86
Jonathan Lun <jonathan.lun23@gmail.com>
2025-02-04 09:55:16 -0500

    move app.jsx to landingPage component

--- myvote.client/src/App.jsx
+++ myvote.client/src/App.jsx
@@ -1,68 +1,11 @@
-import { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css';
-import PollCard from './components/PollCard';
-import CreatePollButton from './components/CreatePollButton';
-import Header from './components/Header';
-import CreatePoll from './pages/CreatePoll';
-import PollDetails from './pages/PollDetails';
+import LandingPage from "./pages/LandingPage";
+
 
 function App() {
-    const API_BASE_URL =
-        window.location.hostname === 'localhost'
-            ? 'https://localhost:7054/api'
-            : 'https://myvote-a3cthpgyajgue4c9.canadacentral-01.azurewebsites.net/api';
-
-    const [polls, setPolls] = useState([]);
-    const [loading, setLoading] = useState(true);
-
-    useEffect(() => {
-        fetchPolls();
-    }, []);
-
-    const contents = loading
-        ? <p><em>Loading... Please refresh once the ASP.NET backend has started.</em></p>
-        : polls.map(poll => <PollCard key={poll.pollId} poll={poll} />);
-
     return (
-        <Router>
-            <div className="main-container">
-                <Header />
-                
-              
-                <div className="main-content">
-                    <Routes>
-                        <Route path="/createpoll" element={<CreatePoll />} />
-                        <Route path="/" element={
-                            <>
-                                <h1>MyVote</h1>
-                                <div className="polls-container">
-                                    {contents}
-                                    <CreatePollButton />
-                                </div>
-                            </>
-                        } />
-                        <Route path="/poll/:pollId" element={<PollDetails />} />
-                    </Routes>
-                </div>
-            </div>
-        </Router>
+        <LandingPage></LandingPage>
     );
-
-    async function fetchPolls() {
-        try {
-            const response = await fetch(`${API_BASE_URL}/polls`);
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            const data = await response.json();
-            setPolls(data);
-        } catch (error) {
-            console.error('There was a problem with the fetch operation:', error.message);
-        } finally {
-            setLoading(false);
-        }
-    }
+    
 }
 
 export default App;
---
+++ myvote.client/src/pages/LandingPage.jsx
@@ -0,0 +1,37 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './LandingPage.css';
+import CreatePollButton from '../components/CreatePollButton';
+import Header from '../components/Header';
+import CreatePoll from './CreatePoll';
+import PollDetails from './PollDetails';
+
+function LandingPage() {
+
+    return (
+        <Router>
+            <div className="main-container">
+                <Header />
+                
+            
+                <div className="main-content">
+                    <Routes>
+                        <Route path="/createpoll" element={<CreatePoll />} />
+                        <Route path="/" element={
+                            <>
+                                <h1>MyVote</h1>
+                                <div className="polls-container">
+                                    <CreatePollButton />
+                                </div>
+                            </>
+                        } />
+                        <Route path="/poll/:pollId" element={<PollDetails />} />
+                    </Routes>
+                </div>
+            </div>
+        </Router>
+    );
+
+
+}
+
+export default LandingPage;
