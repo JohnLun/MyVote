d0e3522fdab2354d95e28e4b1f7d823cf3e80d46
Jonathan Lun <jonathan.lun23@gmail.com>
2025-02-05 11:39:18 -0500

    home icon + nav to landing page

--- myvote.client/src/components/Header.jsx
+++ myvote.client/src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Header.css';
-import { FaUserCircle, FaFilter } from 'react-icons/fa';
+import { FaUserCircle, FaHome, FaFilter } from 'react-icons/fa';
 import { useNavigate } from 'react-router-dom';
 
 function Header() {
@@ -10,10 +10,17 @@ function Header() {
         navigate('/user'); // Replace '2' with the actual user ID if needed
     };
 
+    const handleHomeIconClick = () => {
+        navigate('/');
+    };
+
     return (
         <header className="header">
             <div className="header-icon" onClick={handleUserIconClick}>
                 <FaUserCircle size={24} />
+            </div>
+            <div className="home-icon">
+                <FaHome size={24} onClick={handleHomeIconClick}></FaHome>
             </div>
             
             
