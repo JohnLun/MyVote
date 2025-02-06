5e1ec8dc835418f75fd1720a2c1ac55782a2b425
Jonathan Lun <jonathan.lun23@gmail.com>
2025-02-06 11:30:14 -0500

    added share link

--- myvote.client/src/components/PollCard.jsx
+++ myvote.client/src/components/PollCard.jsx
@@ -1,22 +1,34 @@
 import React from 'react';
+import { FaPaperPlane } from "react-icons/fa";
+import { useNavigate } from 'react-router-dom';
 import './PollCard.css';
-import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
 
-export default function PollCard({poll}) {
-  const navigate = useNavigate();
-  const handleGoClick = () => {
-    if (poll.pollId) {
-        navigate(`/poll/${poll.pollId}`);
-    }
-  };
-  return (
-    <div className="poll-card"
-      onClick={handleGoClick}
-    >
-      <h2>{poll.title}</h2>
-      <p>{poll.description}</p>
-      <p>Time Limit: {new Date(poll.timeLimit).toLocaleString()}</p>
-      <p>Status: {poll.isActive == "t" ? 'Active' : 'Inactive'}</p>
-    </div>
-  );
+export default function PollCard({ poll }) {
+    const navigate = useNavigate();
+
+    const handleGoClick = () => {
+        if (poll.pollId) {
+            navigate(`/poll/${poll.pollId}`);
+        }
+    };
+
+    const handleShareClick = (event) => {
+        event.stopPropagation(); // Prevents the poll card click event
+        navigate(`/poll-link/${poll.pollId}`);
+    };
+
+    return (
+        <div className="poll-card" onClick={handleGoClick}>
+            <div className="poll-header">
+                <h2>{poll.title}</h2>
+                <FaPaperPlane
+                    className="poll-icon"
+                    onClick={handleShareClick}
+                />
+            </div>
+            <p>{poll.description}</p>
+            <p>Time Limit: {new Date(poll.timeLimit).toLocaleString()}</p>
+            <p>Status: {poll.isActive === "t" ? 'Active' : 'Inactive'}</p>
+        </div>
+    );
 }
