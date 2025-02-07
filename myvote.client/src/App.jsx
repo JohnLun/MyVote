1a0da8f9d9cdfce0ddc80992f21ea37b7fb600ce
Jonathan Lun <jonathan.lun23@gmail.com>
2025-02-07 12:17:48 -0500

    fixed ui issues

--- myvote.client/src/App.jsx
+++ myvote.client/src/App.jsx
@@ -11,18 +11,27 @@ import Header from './components/Header';
 function App() {
 
     return (
+        
            <Router>
                 <UserProvider>
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/createpoll" element={<CreatePoll />} />
-                        <Route path="/poll/:pollId" element={<PollDetails />} />
-                        <Route path="/user" element={<UserProfile />} />
-                        <Route path="/poll-link/:pollId" element={<PollLinkPage />} />
-                    </Routes>
+                    <div className="main-container">
+                        <Header />
+                        <div className="main-content">
+                            <Routes>
+                                <Route path="/" element={<Home />} />
+                                <Route path="/createpoll" element={<CreatePoll />} />
+                                <Route path="/poll/:pollId" element={<PollDetails />} />
+                                <Route path="/user" element={<UserProfile />} />
+                                <Route path="/poll-link/:pollId" element={<PollLinkPage />} />
+                            </Routes>
+                        </div>
+                    </div>
                 </UserProvider>
             </Router>
-    ); 
+        
+        
+    );
+    
 }
 
 export default App;
